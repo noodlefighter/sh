e9f116a4552e8360bd8b9db5c669f927598194db
noodlefighter <rongjiayu@gmail.com>
2017-02-12 20:13:51 +0800

    ubuntu sh kernel 4.9.9

--- ubuntu/kernel_update.sh
+++ ubuntu/kernel_update.sh
@@ -2,9 +2,9 @@
 
 # 换内核
 # 下载最新内核,最新内核查看这里 http://kernel.ubuntu.com/~kernel-ppa/mainline
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.7/linux-image-4.9.7-040907-generic_4.9.7-040907.201702010331_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.9/linux-image-4.9.9-040909-generic_4.9.9-040909.201702090333_i386.deb
 
-dpkg -i linux-image-4.9.7*.deb
+dpkg -i linux-image-4.9.9*.deb
 
 update-grub
 reboot

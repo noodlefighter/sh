574d6a3fb3d4cd4235912a398f993c9ab05168f5
noodlefighter <rongjiayu@gmail.com>
2017-03-05 03:06:26 +0800

    权限

--- gitserver/git_autodeploy.sh
+++ gitserver/git_autodeploy.sh
@@ -15,11 +15,12 @@ DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 # 建仓库
 sudo git init --bare $1
 
-# 建立发布目录
+# 建立发布目录 禁止其他用户访问.git
 rm -R $2
 git clone $1 $2
 chown -R git:git $2
-chmod -R 700 $2
+chmod -R 755 $2
+chmod -R 700 $2/.git
 
 # 创建发布hook 设置执行权限
 cp $DIR/_hook_post-receive.txt $1/hooks/post-receive
